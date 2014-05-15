4e04aa44b24ff7cfb400e03c3e51af6cdeab24e4
David Chaves <dchavesd@gmail.com>
2014-05-15 16:20:47 +0200

    Initial commit

---
+++ desktop/src/org/ceremonia/maxpixel/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.ceremonia.maxpixel.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.ceremonia.maxpixel.MaxPixel;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MaxPixel(), config);
+	}
+}
